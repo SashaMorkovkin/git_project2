dfb02c516e6f965e92f192bf17f81e754abe33f8
Aleksander_Morkovkin <sashagus2008@,ail.ru>
2023-11-19 13:23:41 +0300

    forst_commit

--- main.py
+++ main.py
@@ -5,51 +5,11 @@ from PyQt5.QtGui import QPainter, QColor
 import random
 from PyQt5 import uic
 
-template = """<?xml version="1.0" encoding="UTF-8"?>
-<ui version="4.0">
- <class>Form</class>
- <widget class="QWidget" name="Form">
-  <property name="geometry">
-   <rect>
-    <x>0</x>
-    <y>0</y>
-    <width>687</width>
-    <height>726</height>
-   </rect>
-  </property>
-  <property name="windowTitle">
-   <string>Form</string>
-  </property>
-  <widget class="QPushButton" name="button">
-   <property name="geometry">
-    <rect>
-     <x>210</x>
-     <y>630</y>
-     <width>261</width>
-     <height>51</height>
-    </rect>
-   </property>
-   <property name="font">
-    <font>
-     <pointsize>11</pointsize>
-    </font>
-   </property>
-   <property name="text">
-    <string> Push to create circle</string>
-   </property>
-  </widget>
- </widget>
- <resources/>
- <connections/>
-</ui>
-"""
-
 
 class CreateCircle(QMainWindow):
     def __init__(self):
         super().__init__()
-        f = io.StringIO(template)
-        uic.loadUi(f, self)
+        uic.loadUi('UI.ui', self)
         self.do_paint = False
         self.button.clicked.connect(self.paint)
 
